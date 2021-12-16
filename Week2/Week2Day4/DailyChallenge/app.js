57ac7fbcaa1d676fb15be5d2bd7fad06e2af8291
leralys <valerialysko@gmail.com>
2021-12-16 10:55:38 +0200

    Daily challenge w2d4 15122021

--- Week2/Week2Day4/DailyChallenge/app.js
+++ Week2/Week2Day4/DailyChallenge/app.js
@@ -12,51 +12,42 @@
 // * Hello *
 // * World *
 // * in    *
-// * a     *
+// * a---- *
 // * frame *
 // *********
 
 // The number of stars that wraps the sentence, must depend on the length of the longest word.
 
-let words = ["Hello", "rain", "in", "a", "frame"];
+let words = ["Hello", "world", "in", "a", "frame"];
 let asterisk = '*';
 let border = 2; //Must be even; two sides, each is integer
 let padding = 2; //Must be even; two sides, each is integer
-// let firstLastLine;
 
 // find the longest word
 const longestWord = (arr) => {
     let longestLength = 0;
-    for (let word of words) {
-        if (word.length > longestLength) longestLength = word.length;
-        return longestLength;
-    }
+    for (let el of arr) {
+        if (el.length > longestLength) longestLength = el.length;
+    } return longestLength;
 }
-// construct first and last line with needed amount of *
-const firstLast = (arr) => {
-    // using variables just not to forget where the numbers are coming from
-    return asterisk.repeat(longestWord(arr) + border + padding);
-}
-
-let fl = firstLast(words).length;
-let last = firstLast(words);
 
 const addPaddingBorder = (arr) => {
+    let word = longestWord(arr);
+    let len = word + border + padding;
     for (let i = 0; i < arr.length; i++) {
         arr[i] = arr[i].padStart(arr[i].length + padding / 2);
-        arr[i] = arr[i].padEnd(fl - border);
+        arr[i] = arr[i].padEnd(word + padding);
         arr[i] = asterisk.repeat(border / 2) + arr[i] + asterisk.repeat(border / 2);
     } return arr;
 }
 
 const construct = (arr) => {
-    arr.unshift(firstLast());
-    arr.push(firstLast());
-    for (let i = 1; i < arr.length - 1; i++) {
-        console.log(arr[i])
+    let firstLast = asterisk.repeat(9);
+    addPaddingBorder(arr);
+    arr.unshift(firstLast);
+    arr.push(firstLast);
+    for (let el of arr) {
+        console.log(el);
     }
 }
-console.log(firstLast());
-addPaddingBorder(words);
 construct(words);
-console.log(last);
