92d3abebef44ca64a95876910bf3a2fd8e43d647
leralys <valerialysko@gmail.com>
2021-12-19 11:12:29 +0200

    DailyChallenge planets w3d1 without bonus part

---
+++ Week3/Week3Day1/DailyChallenge/app.js
@@ -0,0 +1,19 @@
+// Create an array which value is the planets of the solar system.
+// For each planet in the array, create a <div> using createElement. This div should have a class named "planet".
+// Each planet should have a different background color. (Hint: add a new class to each planet).
+// Finally append each div to the <section> in the HTML (presented below).
+
+// Bonus: Do the same process to create the moons.
+// Be careful, each planet has a certain amount of moons. How should you display them?
+// Should you still use an array for the planets ? Or an array of objects ?
+
+const arr = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+const colors = ['red', 'blue', 'green', 'cyan', 'orange', 'grey', 'pink', 'purple']
+
+for (let i = 0; i < arr.length; i++) {
+    let newDiv = document.createElement('div');
+    newDiv.classList.add('planet', `${colors[i]}`);
+    newDiv.style.backgroundColor = `${colors[i]}`;
+    document.body.firstElementChild.appendChild(newDiv);
+}
+console.log(document.body.firstElementChild.appendChild(newDiv))
