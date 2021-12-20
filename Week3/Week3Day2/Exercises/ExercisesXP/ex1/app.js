9855c7ba66177b7959347066960aae35bc12b8e6
leralys <valerialysko@gmail.com>
2021-12-20 13:02:41 +0200

    exercise1 exercisesxp w3d2 with bonus part

--- Week3/Week3Day2/Exercises/ExercisesXP/ex1/app.js
+++ Week3/Week3Day2/Exercises/ExercisesXP/ex1/app.js
@@ -32,8 +32,32 @@ button.addEventListener('click', () => {
 });
 
 // BONUS : When you hover on the h1, set the font size to a random pixel size between 0 to 100
+
 h1.addEventListener('mouseover', () => {
     h1.style.fontSize = `${Math.floor(Math.random() * 100) + 1}px`;
 })
 
 // BONUS : When you hover on the 2nd paragraph, it should fade out 
+
+const hoverEffect = (event) => {
+    let fadeTarget = event.target;
+    let fadeEffect = setInterval(function () {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 1;
+        }
+        if (fadeTarget.style.opacity > 0) {
+            fadeTarget.style.opacity -= 0.1;
+        } else {
+            clearInterval(fadeEffect);
+        }
+    }, 60)
+}
+
+const returnOpacity = (event) => {
+    let target = event.target;
+    if (target.style.opacity == 0) target.style.opacity = '1';
+
+}
+
+paragraphs[1].addEventListener('mouseenter', hoverEffect);
+paragraphs[1].addEventListener('mouseleave', returnOpacity);
