1194428a9d4d5065a358d7b7f8255c87149b82aa
leralys <valerialysko@gmail.com>
2021-12-22 21:58:01 +0200

    DailyChallenge w3d4 todo list

--- Week3/Week3Day4/DailyChallengeToDo/js/app.js
+++ Week3/Week3Day4/DailyChallengeToDo/js/app.js
@@ -1,5 +1,3 @@
-let listTasks = [];
-
 // Create a function called addTask(). As soon as the user clicks on the button:
 // check that the input is not empty,
 // then add it to the array (ie. add the text of the task)
@@ -9,11 +7,10 @@ let listTasks = [];
 // a “X” button. Use font awesome for the “X” button.
 
 
-
+let listTasks = [];
 const submit = document.querySelector('#submit');
 const myForm = document.forms[0];
 const newTask = document.querySelector('#new-task'); //input of new task
-// const listTasks = document.querySelector('.listTasks');
 const list = document.querySelector('.listTasks');
 
 
@@ -25,10 +22,15 @@ const addTask = function () {
         let checkbox = document.createElement('input'); //create checkbox
         checkbox.setAttribute('type', 'checkbox');
         checkbox.classList.add('checkbox');
+        let icon = document.createElement('i'); //create icon
+        icon.classList.add('far', 'fa-trash-alt', 'icon');
+        let taskText = document.createTextNode(newTask.value);
+        task.append(icon); //append icon
         task.append(checkbox); //append checkbox to the todo
-        let taskText = document.createTextNode(newTask.value); //append text of the todo
-        task.append(taskText);
+        task.append(taskText); //append text of the todo
         list.append(task); //append todo to the list
+        listTasks.push(newTask.value);
+        console.log(listTasks);
         list.style.display = 'block';
         newTask.value = '';
     }
@@ -36,6 +38,5 @@ const addTask = function () {
 
 myForm.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log('clicked');
     addTask();
 });
