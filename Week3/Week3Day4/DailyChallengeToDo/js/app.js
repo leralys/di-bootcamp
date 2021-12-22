4da1e5b027efd6c11cb9c114e17d8ecdfdac73c0
leralys <valerialysko@gmail.com>
2021-12-22 19:15:02 +0200

    DailyChallenge todo list layout and styles

---
+++ Week3/Week3Day4/DailyChallengeToDo/js/app.js
@@ -0,0 +1,22 @@
+// In the js file, you must add the following functionalities:
+// Create an empty array : let listTasks = [];
+
+// Create a function called addTask(). As soon as the user clicks on the button:
+// check that the input is not empty,
+// then add it to the array (ie. add the text of the task)
+// then add it to the DOM, below the form (in the <div class="listTasks"></div>) .
+// Each new task added should have:
+// an input type="checkbox
+// a “X” button. Use font awesome for the “X” button.
+
+// BONUS I (not mandatory):
+// Change the variable listTasks to an array of task objects.
+// Each new task added to the array should have the properties : task_id, text and done (a boolean - false by default).
+// Every new task object should have a task_id, starting from 0, and a data-task-id attribute, which value is the same as the task_id. Check out data-* attributes here.
+// Create a function named doneTask(), that as soon as the user clicks on the “checkbox” input, the done property should change from false to true in the object, and from black to crossed out red in the DOM.
+
+// BONUS II (not mandatory):
+// Create a function named deleteTask(), that as soon as the user clicks on the “X” button, delete that specific task from the array listTasks.
+
+
+// console.log('Hi');
