7e940791bde7f54720ef209e888c709e443a905a
leralys <valerialysko@gmail.com>
2021-12-26 12:29:28 +0200

    daily challenge w3d5

--- Week3/Week3Day5/DailyChallenge/app.js
+++ Week3/Week3Day5/DailyChallenge/app.js
@@ -1,20 +1,19 @@
-
-
-
 let num = 7;
 const grid = document.querySelector('.grid')
-for (let i = 0; i < num; i++) {
-    for (let j = 0; j < num; j++) {
-        let div = document.createElement('div');
-        div.classList.add('cell');
-        grid.append(div);
-        if (
-            ((j === 0 || j === Math.floor(num / 2)) && i !== 0) ||
-            (i === 0 && j !== 0 && j !== Math.floor(num / 2)) ||
-            i === Math.floor(num / 2)
-        ) {
-            div.innerText = '*';
-        } else div.innerText = '   ';
+const makeA = (num) => {
+    let middle = Math.floor(num / 2)
+    for (let i = 0; i < num; i++) {
+        for (let j = 0; j < num; j++) {
+            let div = document.createElement('div');
+            div.classList.add('cell');
+            grid.append(div);
+            // horizontal lines
+            if ((i == 0 || i == middle) && (j >= middle - 1 && j <= middle + 1)) div.innerText = '*';
+            // vertical lines
+            if ((j == 1 || j == 5) && i >= 1)
+                div.innerText = '*';
+        }
     }
 }
 
+makeA(7)
