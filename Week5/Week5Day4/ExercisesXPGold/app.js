bdb5483a30ea7c13e4d2e82d3d7f6d79a294cde2
leralys <valerialysko@gmail.com>
2022-01-09 23:21:37 +0200

    todo

---
+++ Week5/Week5Day4/ExercisesXPGold/app.js
@@ -0,0 +1,94 @@
+// Exercise 1: Analyze #2
+// Instructions
+// Analyze the code provided above what will be the output?
+
+let resolveAfter2Seconds = function () {
+    console.log("starting slow promise");
+    return new Promise(resolve => {
+        setTimeout(function () {
+            resolve("slow");
+            console.log("slow promise is done");
+        }, 2000);
+    });
+};
+
+let resolveAfter1Second = function () {
+    console.log("starting fast promise");
+    return new Promise(resolve => {
+        setTimeout(function () {
+            resolve("fast");
+            console.log("fast promise is done");
+        }, 1000);
+    });
+};
+
+let sequentialStart = async function () {
+    console.log('==SEQUENTIAL START==');
+    const slow = await resolveAfter2Seconds();
+    console.log(slow);
+    const fast = await resolveAfter1Second();
+    console.log(fast);
+}
+
+sequentialStart()
+
+
+
+// Exercise 2: Analyze #3
+// Instructions
+// Analyze the code provided above what will be the output?
+
+let resolveAfter2Seconds = function () {
+    console.log("starting slow promise");
+    return new Promise(resolve => {
+        setTimeout(function () {
+            resolve("slow");
+            console.log("slow promise is done");
+        }, 2000);
+    });
+};
+
+let resolveAfter1Second = function () {
+    console.log("starting fast promise");
+    return new Promise(resolve => {
+        setTimeout(function () {
+            resolve("fast");
+            console.log("fast promise is done");
+        }, 1000);
+    });
+};
+
+let concurrentStart = async function () {
+    console.log('==CONCURRENT START with await==');
+    const slow = resolveAfter2Seconds();
+    const fast = resolveAfter1Second();
+    console.log(await slow);
+    console.log(await fast);
+}
+
+setTimeout(concurrentStart, 4000)
+
+
+
+// Exercise 3 : Modify Fetch With Async/Await
+// Instructions
+// Using this code:
+// Modify the function above. Add async await in place of the following line:
+// fetch(url).then(resp => resp.json())
+// So there shouldn’t be any .then() calls anymore!
+// Don’t get discouraged… this is a really tough one…
+
+const urls = [
+    "https://jsonplaceholder.typicode.com/users",
+    "https://jsonplaceholder.typicode.com/posts",
+    "https://jsonplaceholder.typicode.com/albums"
+];
+
+const getData = async function () {
+    const [users, posts, albums] = await Promise.all(urls.map(url =>
+        fetch(url).then(resp => resp.json())
+    ));
+    console.log('users', users);
+    console.log('posta', posts);
+    console.log('albums', albums);
+}
