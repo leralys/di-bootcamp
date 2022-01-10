893cfffea43d55247198615ab207af9e95096579
leralys <valerialysko@gmail.com>
2022-01-10 10:28:28 +0200

    fix

--- Week6/Week6Day1/project_lise.sql
+++ Week6/Week6Day1/project_lise.sql
@@ -21,6 +21,8 @@
 
 -- 3. When (ie. days and hours) was the temperature between 28 and 30 degrees in San Francisco ?
 -- SELECT event_datetime from city_info WHERE temperature >=28 AND temperature <=30;
+-- SELECT event_datetime FROM city_info WHERE temperature BETWEEN 28 AND 30 AND city = 'San Francisco';
+-- SELECT EXTRACT(DAY FROM event_datetime), EXTRACT(MONTH FROM event_datetime), EXTRACT(HOUR FROM event_datetime) FROM city_info WHERE temperature BETWEEN 28 AND 30 AND city = 'San Francisco';
 
 -- 4. Which city was the most noisy (show the name of the city, the date and the noise index)? - Don't use the MAX function
 -- change the sign, find min, change sign again
@@ -33,9 +35,10 @@
 -- SELECT dust FROM city_info WHERE city = 'San Francisco' AND event_datetime > '2015-03-15 11:00:00';
 
 -- 7. When (ie. days and hours) is the humidity index less that 40 or more than 60 in Geneva?
--- SELECT event_datetime, humidity FROM city_info WHERE city = 'Geneva' AND humidity < 40 OR humidity > 60;
+-- SELECT EXTRACT(DAY FROM event_datetime), EXTRACT(MONTH FROM event_datetime), EXTRACT(HOUR FROM event_datetime), humidity FROM city_info WHERE city = 'Geneva' AND (humidity < 40 OR humidity > 60);
 
 -- 8. Where and when (Find the day (Sunday-Saturday)) is the light index the highest? - Use an alias for the day of the week.
+-- SELECT EXTRACT (DOW FROM event_datetime) AS dayOfWeek, city FROM city_info ORDER BY light LIMIT 1; 
 
 -- 9. Select only the info of the cities that start with an "S" (uppercase or lowercase). - Look at the DISTINCT constraint.
 -- SELECT DISTINCT city FROM city_info WHERE city ILIKE 's%';
@@ -54,7 +57,7 @@
 -- 	humidity,
 -- 	dust)
 -- VALUES (
--- 	'2022-01-09 13:32:00',
+-- 	NOW()
 -- 	'Tel Aviv',
 -- 	19.938737484,
 -- 	4080.07673500435,
@@ -63,3 +66,11 @@
 -- 	44.335284579349,
 -- 	1051.88421970685)
 -- RETURNING *;
+
+
+
+-- Extract the date from Timestamp
+-- Extract date from a specific timestamp value:
+-- Cast the timestamp to a date by adding ::date suffix:
+-- SELECT '2018-07-25 10:30:30'::TIMESTAMP::DATE;
+
