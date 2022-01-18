2490e136b1a8cba1f6a80ed81fec605bb5e526d1
leralys <valerialysko@gmail.com>
2022-01-18 18:38:02 +0200

    exercise xp file system

--- Week7/Week7Day3/ExerciseXP/fs.js
+++ Week7/Week7Day3/ExerciseXP/fs.js
@@ -33,9 +33,9 @@ const fs = require('fs');
 // })
 
 
-fs.unlink('./data.txt', err => {
-    if (err) {
-        console.log(err);
-    }
-    console.log('deleted');
-})
+// fs.unlink('./data.txt', err => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log('deleted');
+// })
