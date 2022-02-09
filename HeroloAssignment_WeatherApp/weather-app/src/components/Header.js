b01fdbac1dfd822154faf94e36e802e1be47cf11
leralys <valerialysko@gmail.com>
2022-02-09 22:49:24 +0200

    active routes

--- HeroloAssignment_WeatherApp/weather-app/src/components/Header.js
+++ HeroloAssignment_WeatherApp/weather-app/src/components/Header.js
@@ -1,50 +1,33 @@
-// import { useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import HomeIcon from '@mui/icons-material/Home';
-// import FavoriteIcon from '@mui/icons-material/Favorite';
-// import weatherLogo from '../assets/images/weatherLogo.png';
-// import './styles/Header.css';
-
-// const Header = () => {
-//     return (
-//         <header className='Header'>
-//             <Link id='Logo' to={'/'}>Weather App</Link>
-//             <nav>
-//                 <Link to={'/'}><HomeIcon fontSize='large' className='Header-navIcon' /></Link>
-//                 <Link to={'/favorites'}><FavoriteIcon fontSize='large' className='Header-navIcon' /></Link>
-//             </nav>
-//         </header>
-//     );
-// }
-
-// export default Header;
-
-
-
-
-
-
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import HomeIcon from '@mui/icons-material/Home';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import './styles/Header.css';
 
-export default function ButtonAppBar() {
+const Header = () => {
+    let activeStyle = {
+        opacity: '1'
+    };
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position='static' className='Header'>
                 <Toolbar>
-                    <Link id='Logo' to={'/'}><Button color='inherit'>Weather App</Button></Link>
+                    <Link id='logo' to={'/'}><Button color='inherit'>Weather App</Button></Link>
                     <nav>
-                        <Link to={'/'}><Button color='inherit'><HomeIcon fontSize='large' /></Button></Link>
-                        <Link to={'/favorites'}><Button color='inherit'><FavoriteIcon fontSize='large' /></Button></Link>
+                        <NavLink to='/' style={({ isActive }) => isActive ? activeStyle : undefined}>
+                            <Button color='inherit'><HomeIcon fontSize='large' /></Button>
+                        </NavLink>
+                        <NavLink to='/favorites' style={({ isActive }) => isActive ? activeStyle : undefined}>
+                            <Button color='inherit'><FavoriteIcon fontSize='large' /></Button>
+                        </NavLink>
                     </nav>
                 </Toolbar>
             </AppBar>
         </Box>
     );
 }
+
+export default Header;
